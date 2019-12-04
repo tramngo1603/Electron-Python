cbf5fab46447441b4ebebecfdd006fc65aa629ba
Tram Ngo <tramngo1603@gmail.com>
2019-12-03 17:40:35 -0800

    Return Sum to Output box

--- dist/renderer.js
+++ dist/renderer.js
@@ -2,12 +2,11 @@ const path = require('path')
 
 let box1 = document.querySelector('#box1')
 let box2 = document.querySelector('#box2')
-let result_box = document.querySelector('#result')
 let button = document.querySelector('#my_button')
+
 
 button.addEventListener('click', () => {
   let script = path.join(__dirname, 'add.py')
   let stdout = require('child_process').execSync('python "' + script + '" ' + box1.value + ' ' + box2.value)
-  console.log(stdout)
-  result.textContent = stdout
+  let element = document.getElementById("result").value = stdout
 })
