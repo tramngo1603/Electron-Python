011ad3cb834049233e23459f8aab50af4fb7b904
Tram Ngo <tramngo1603@gmail.com>
2019-12-03 19:01:16 -0800

    Use python executable instead of script

--- renderer.js
+++ renderer.js
@@ -6,7 +6,8 @@ let button = document.querySelector('#my_button')
 
 
 button.addEventListener('click', () => {
-  let script = path.join(__dirname, 'add.py')
-  let stdout = require('child_process').execSync('python "' + script + '" ' + box1.value + ' ' + box2.value)
+  let pyExe = path.join(__dirname, 'dist', 'add.exe')
+  // "" around pyExe to handle spaces in path
+  let stdout = require('child_process').execSync('"' + pyExe + '" ' + box1.value + ' ' + box2.value)
   let element = document.getElementById("result").value = stdout
 })
