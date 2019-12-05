9fc3ebecd5bc53a262384f8c7549f7923435a6b0
Tram Ngo <tramngo1603@gmail.com>
2019-12-05 14:12:12 -0800

    Call Zerorpc Sum_func

--- renderer.js
+++ renderer.js
@@ -1,4 +1,16 @@
 const path = require('path')
+const zerorpc = require("zerorpc")
+let client = new zerorpc.Client()
+
+client.connect("tcp://127.0.0.1:4242")
+
+client.invoke("echo", "server ready", (error, res) => {
+  if(error || res !== 'server ready') {
+    console.error(error)
+  } else {
+    console.log("server is ready")
+  }
+})
 
 let box1 = document.querySelector('#box1')
 let box2 = document.querySelector('#box2')
@@ -6,8 +18,12 @@ let button = document.querySelector('#my_button')
 
 
 button.addEventListener('click', () => {
-  let pyExe = path.join(__dirname, 'dist', 'add.exe')
-  // "" around pyExe to handle spaces in path
-  let stdout = require('child_process').execSync('"' + pyExe + '" ' + box1.value + ' ' + box2.value)
-  let element = document.getElementById("result").value = stdout
+  client.invoke("sum_func", box1.value, box2.value, (error, res) => {
+    if(error) {
+      console.error(error)
+    } else {
+        document.getElementById("result").value = res
+    }
+  })
+
 })
