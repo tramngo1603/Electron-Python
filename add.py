e2ad16101212d79ccf6c2449d89427c8985629a4
Tram Ngo <tramngo1603@gmail.com>
2019-12-04 17:20:05 -0800

    Run zerorpc server

--- add.py
+++ add.py
@@ -1,11 +1,19 @@
 import argparse
+import zerorpc
 
-def sum_func(a,b):
-    return a+b
+class SumAPI():
+    def sum_func(self,a,b):
+        return a+b
+
+def main(port):
+    addr = 'tcp://127.0.0.1:' + port
+    s = zerorpc.Server(SumAPI())
+    s.bind(addr)
+    print('start running on {}'.format(addr))
+    s.run()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('num1', help='First number to add')
-    parser.add_argument('num2', help='Second number to add')
+    parser.add_argument('port', help='Port for Zerorpc to listen on')
     args = parser.parse_args()
-    print(sum_func(float(args.num1), float(args.num2)))
+    main(args.port)
