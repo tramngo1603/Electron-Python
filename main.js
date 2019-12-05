b91c757f537a73aed3a68a9f3e68594c93608545
Tram Ngo <tramngo1603@gmail.com>
2019-12-05 14:10:29 -0800

    Start Zerorpc Python server

--- main.js
+++ main.js
@@ -2,6 +2,38 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require('path')
+const child_process = require('child_process')
+
+let pyProc = null
+let pyPort = null
+
+const selectPort = () => {
+  pyPort = 4242
+  return pyPort
+}
+
+const createPyProc = () => {
+  let pyExe = path.join(__dirname, 'dist', 'add.exe')
+  let port = '' + selectPort()
+
+  // "" around pyExe to handle spaces in path
+  pyProc = child_process.execFile('"' + pyExe + '" ', [port])
+
+  if (pyProc != null) {
+    //console.log(pyProc)
+    console.log('child process success on port ' + port)
+  }
+}
+
+const exitPyProc = () => {
+  pyProc.kill()
+  pyProc = null
+  pyPort = null
+}
+
+app.on('ready', createPyProc)
+app.on('will-quit', exitPyProc)
+
 
 // Initialize a window
 
